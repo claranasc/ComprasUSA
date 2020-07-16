6cf637f48b595b2f27ffc7bdc50275cc1095cf0e
Clara Nascimento <clara.nascimento@outlook.com>
2020-07-16 09:28:57 -0300

    Add TabBarController, add class, add elements in ShoppingScene

---
+++ ComprasUSA/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  ComprasUSA
+//
+//  Created by Clara Nascimento on 16/07/20.
+//  Copyright © 2020 Clara Nascimento. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
