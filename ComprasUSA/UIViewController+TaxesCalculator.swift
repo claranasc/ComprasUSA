ecd132110fea37a00fe1d2ac0446a9bd35e6e72c
Clara Nascimento <clara.nascimento@outlook.com>
2020-07-22 19:29:11 -0300

    Aula 76 - Add file UIViewController+TaxesCalculator, import UIKit, extension UIViewController, ajustado o teclado dos TextFields, parte 3

---
+++ ComprasUSA/UIViewController+TaxesCalculator.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewController+TaxesCalculator.swift
+//  ComprasUSA
+//
+//  Created by Clara Nascimento on 22/07/20.
+//  Copyright © 2020 Clara Nascimento. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    var tc: TaxesCalculator {
+        return TaxesCalculator.shared
+    }
+}
