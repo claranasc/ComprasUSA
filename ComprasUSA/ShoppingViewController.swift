8c9ded65f6a3a979fbb2a828632c83cbc869f3c7
Clara Nascimento <clara.nascimento@outlook.com>
2020-07-22 19:04:09 -0300

    Aula 76 - Add outlets for all TextFields, Labels and Swift, parte 1

--- ComprasUSA/ShoppingViewController.swift
+++ ComprasUSA/ShoppingViewController.swift
@@ -1,7 +1,7 @@
 //
 //  ViewController.swift
 //  ComprasUSA
-//
+//  
 //  Created by Clara Nascimento on 15/07/20.
 //  Copyright © 2020 Clara Nascimento. All rights reserved.
 //
@@ -10,11 +10,13 @@ import UIKit
 
 class ShoppingViewController: UIViewController {
 
+    @IBOutlet weak var tfDolar: UITextField!
+    @IBOutlet weak var lbRealDescription: UILabel!
+    @IBOutlet weak var lbReal: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
