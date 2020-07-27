3b025d9e3273880768afb63386aea15bd6a0c8f5
Clara Nascimento <clara.nascimento@outlook.com>
2020-07-27 19:29:38 -0300

    Aula 77 - Implementei métodos e cálculos na TaxesViewController e  ajustei um item no Storyboard, parte 6

--- ComprasUSA/TaxesViewController.swift
+++ ComprasUSA/TaxesViewController.swift
@@ -18,11 +18,29 @@ class TaxesViewController: UIViewController {
     @IBOutlet weak var swCreditCard: UISwitch!
     @IBOutlet weak var lbReal: UILabel!
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        calculateTaxes()
+    }
+    
+    @IBAction func changeIOF(_ sender: UISwitch) {
+        calculateTaxes()
+    }
+    
+    func calculateTaxes() {
+        lbStateTaxDescription.text = "Imposto do Estado (\(tc.getFormattedValue(of: tc.stateTax, withCurreny: ""))%)"
+        lbIOFDescription.text = "IOF(\(tc.getFormattedValue(of: tc.iof, withCurreny: ""))%)"
+        
+        lbDolar.text = tc.getFormattedValue(of: tc.shoppingValue, withCurreny: "US$ ")
+        lbStateTax.text = tc.getFormattedValue(of: tc.stateTaxValue, withCurreny: "US$ ")
+        lbIOF.text = tc.getFormattedValue(of: tc.iofValue, withCurreny: "US$ ")
+        
+        let real = tc.calculate(usingCreditCard: swCreditCard.isOn)
+        lbReal.text = tc.getFormattedValue(of: real, withCurreny: "R$ ")
+        
     }
 }
